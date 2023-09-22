e574de661b8b4f69eef2df42c47537c8157592a4
jgirish23 <jakkalagirish@gmail.com>
2023-09-23 00:26:52 +0530

    fixed linting issue in cal, wait_for_db, cal and tests

--- app/app/cal.py
+++ app/app/cal.py
@@ -2,8 +2,10 @@
 calculator
 """
 
+
 def add(x, y):
     return x + y
+
 
 def sub(x, y):
     return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ test cal
 from django.test import SimpleTestCase
 from app import cal
 
+
 class CalTests(SimpleTestCase):
     """Test the cal module."""
 
